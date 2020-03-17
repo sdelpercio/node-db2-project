ac1c908b0d128656c00ce66c431bd2688fe58e38
Shawn DelPercio <sdelperc@gmail.com>
2020-03-17 18:04:49 -0400

    added endpoints for get requests

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const db = require('../data/dbConfig');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.status(200).json({ message: 'all good' });
+	db('cars')
+		.then(cars => {
+			res.status(200).json(cars);
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'Error retrieving cars.' });
+		});
+});
+router.get('/:id', (req, res) => {
+	db('cars')
+		.where({ id: req.params.id })
+		.then(car => {
+			if (car.length === 0) {
+				res.status(404).json({ message: 'Car could not be found' });
+			} else {
+				res.status(200).json(car);
+			}
+		});
 });
 
 module.exports = router;
